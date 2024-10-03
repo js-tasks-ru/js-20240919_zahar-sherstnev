298dbf2f2e1bb62040758debbdb4b3106a38f3c0
Zakhar Sherstnev <zakharsherstnev@MacBook-Pro-Zakhar.local>
2024-10-03 18:19:32 +0300

    les 3

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,14 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (obj === undefined) {
+    return undefined;
+  }
 
+  const invertedObj = {};
+
+  for (const key in obj) {
+    invertedObj[obj[key]] = key;
+  }
+  return invertedObj;
 }
